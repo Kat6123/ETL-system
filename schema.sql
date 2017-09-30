38c83ced9aa8dc0601286c8ab4a45df8ac08f622
Katya <kat6123@yandex.ru>
2017-10-01 00:02:18 +0300

    Add sql script which create schema

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP SCHEMA if exists hello CASCADE;
+
+CREATE SCHEMA hello;
+
+CREATE TABLE hello."Jobs"
+(
+    job_id BIGSERIAL PRIMARY KEY,
+    job_title character varying(150),
+    category character varying(100),
+    status character varying(100),
+    location character varying(100)
+)
