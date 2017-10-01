10bca7fa24f9a9d5ab225f0187755075346937f3
Katya <kat6123@yandex.ru>
2017-10-01 14:17:42 +0300

    Add ETL script

--- ETL.py
+++ ETL.py
@@ -2,23 +2,23 @@
 # -*- coding: utf-8 -*-
 
 
-import psycopg2
-import csv
+import sys
+
+from etl_functionality import (
+    download_html,
+    download_to_bd,
+    extract_into_csv
+)
+
+SITE_URL = "https://www.snap.com/en-US/jobs/"
+HTML_FILE = "snap.html"
+CSV_FILE = "snap.csv"
 
 
 def main():
-    with psycopg2.connect("dbname='kat' user='kat' "
-                          "host='localhost' password='3g1o4h5t'") as db:
-        cur = db.cursor()
-        with open("jobs.csv") as fp:
-            csv_reader = csv.reader(fp)
-            for row in csv_reader:
-                cur.execute("""INSERT into snap_jobs."Jobs"
-                            (job_title, category, status, location)
-                            VALUES ('{0}', '{1}', '{2}', '{3}')""".format(
-                                row[0], row[1], row[2], row[3]
-                            ))
-        db.commit()
+    download_html(SITE_URL, HTML_FILE)
+    extract_into_csv(HTML_FILE, CSV_FILE)
+    download_to_bd(CSV_FILE)
 
 
 if __name__ == '__main__':
