1d2442409cee32d7d0e633b503de3aa855ccd35c
Katya <kat6123@yandex.ru>
2017-10-01 14:16:01 +0300

    Update functionality

--- etl_functionality.py
+++ etl_functionality.py
@@ -8,11 +8,7 @@ import csv
 import bs4
 import psycopg2
 
-
-BROWSER_JS_EXEC_PAUSE = 2
-DEFAULT_ENCODING = 'utf-8'
-CSV_HEADER = ('Job Title', 'Category', 'Status', 'Location')
-HTML_ROW_TAG = 'tr'
+import settings
 
 
 def download_html(site_url, output_file):
@@ -20,13 +16,13 @@ def download_html(site_url, output_file):
         with contextlib.closing(selenium.webdriver.Chrome()) as browser:
             browser.get(site_url)
 
-            time.sleep(BROWSER_JS_EXEC_PAUSE)
+            time.sleep(settings.BROWSER_JS_EXEC_PAUSE)
 
             browser.execute_script("window.scrollTo "
                                    "(0, document.body.scrollHeight);")
 
             with open(output_file, "w") as fp:
-                fp.write(browser.page_source.encode(DEFAULT_ENCODING))
+                fp.write(browser.page_source.encode(settings.DEFAULT_ENCODING))
 
 
 def extract_into_csv(input_html, output_csv):
@@ -36,25 +32,30 @@ def extract_into_csv(input_html, output_csv):
     with open(output_csv, "w") as fp:
         csv_writer = csv.writer(fp)
 
-        csv_writer.writerow(CSV_HEADER)
+        csv_writer.writerow(settings.CSV_HEADER)
 
-        for row in soup.find_all(HTML_ROW_TAG):
+        for row in soup.find_all('tr'):
             row_to_write = tuple(
-                (r.encode(DEFAULT_ENCODING) for r in row.stripped_strings)
+                (r.encode(settings.DEFAULT_ENCODING)
+                    for r in row.stripped_strings)
             )
             csv_writer.writerow(row_to_write)
 
 
 def download_to_bd(input_csv):
-    with psycopg2.connect("dbname='kat' user='kat' "
-                          "host='localhost' password='3g1o4h5t'") as db:
+    with psycopg2.connect(**(settings.DATABASE_SETTINGS)) as db:
         cur = db.cursor()
+
+        query = """INSERT into snap_jobs."Jobs"
+                (job_title, category, status, location)
+                VALUES ('{}', '{}', '{}', '{}')"""
+
         with open(input_csv) as fp:
             csv_reader = csv.reader(fp)
+
+            csv_reader.next()
+
             for row in csv_reader:
-                cur.execute("""INSERT into snap_jobs."Jobs"
-                            (job_title, category, status, location)
-                            VALUES ('{0}', '{1}', '{2}', '{3}')""".format(
-                                row[0], row[1], row[2], row[3]
-                            ))
+                cur.execute(query.format(*row))
+
         db.commit()
