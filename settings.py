2d6f22bfbb842fc6d1fa79c5b02be5df23e63a7d
Katya <kat6123@yandex.ru>
2017-10-01 14:15:25 +0300

    Add settings file

---
+++ settings.py
@@ -0,0 +1,10 @@
+BROWSER_JS_EXEC_PAUSE = 2
+DEFAULT_ENCODING = 'utf-8'
+CSV_HEADER = ('Job Title', 'Category', 'Status', 'Location')
+DATABASE_SETTINGS = {
+    'database': '',
+    'user': '',
+    'password': '',
+    'host': 'localhost',
+    'port': '5432'
+}
